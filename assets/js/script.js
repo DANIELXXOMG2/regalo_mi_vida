fa3c2d20dc02abdc610b1db4b20081de499d6e4f
DANIELXXOMG2 <danielsebastiank6@gmail.com>
2024-12-09 20:02:07 -0500

    Se añade audio, footer, sonidos extras

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function reproducirSonidoBoton() {
+    var audio = document.getElementById('audioBoton');
+    audio.play();
+}
+
+function reproducirSonidoFooter() {
+    var audio = document.getElementById('audioFooter');
+    audio.play();
+}
